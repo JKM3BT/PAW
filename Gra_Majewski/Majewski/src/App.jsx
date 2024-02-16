f933d00fa5f43fb073f32d1653dac6846d91077f
Jakub Majewski <jakub.majewski@cyfroweszkoly.pl>
2024-02-16 23:42:58 +0100

    buggy as check, i don't care

--- Gra_Majewski/Majewski/src/App.jsx
+++ Gra_Majewski/Majewski/src/App.jsx
@@ -2,54 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let i = 0;
-  const [points, setPoints] = useState(0)
+  const [Counter, setCounter] = useState(0);
+  const [Points, setPoints] = useState(0);
   const listOfQuestions = ["","Nazwa aktualnej stolicy Polski","Największym (powierzchniowo) państwem na świecie jest...",
   "Imię i nazwisko człowieka uznanego za odkrywcę Ameryki","Jak się mówi poprawnie: Lubuskie, czy Lubelskie?",
   "Wyspa, na której żyją Kangury?","Jaka jest poprawa forma: Arktyka, Antarktyka, czy Antarktyda?"];
 
-  const listOfAnswers = ["","warszawa","rosja","krzysztof kolumb","to są dwa różne województwa","australia","wszystkie"];
+  const listOfAnswers = ["","warszawa","rosja","krzysztof kolumb","oba","australia","wszystkie"];
 
-  const [question, setQuestion] = useState(listOfQuestions[i]);
-  const [answer, setAnswer] = useState(listOfAnswers[i]);
+  const [Question, setQuestion] = useState(listOfQuestions[Counter]);
+  const [Answer, setAnswer] = useState(listOfAnswers[Counter]);
 
   function showTime(){
-    i=1;
-    setQuestion(listOfQuestions[i]);
+    setCounter(1);
+    setQuestion(listOfQuestions[Counter]);
     setPoints(0);
-    answer_here = document.getElementById("answer_box").innerHTML = "";
-  } 
-
-  function next(){
+    let answer_here = document.getElementById("answer_box").value = "";
+    let pytanie_here = document.getElementById("pytanie").innerHTML = Question;
+  }
+  function nextQuestion(){
     answer_here = document.getElementById("answer_box").value;
-    if (answer_here == answer){
-      setPoints(points+1);
-      i++;
-      setQuestion(listOfQuestions[i]);
-      setAnswer(listOfAnswers[i]);
-      pytanie_here = document.getElementById("pytanie").innerHTML = question;
+    console.log(Counter);
+    if (answer_here.toLowerCase() == Answer){
+      setPoints(Points + 1);
+      setCounter(Counter + 1);
+      setQuestion(listOfQuestions[Counter]);
+      setAnswer(listOfAnswers[Counter]);
+      let pytanie_here = document.getElementById("pytanie").innerHTML = Question;
+      answer_here = document.getElementById("answer_box").value = "";
     }
     else{
-      i++;
-      setQuestion(listOfQuestions[i]);
-      pytanie_here = document.getElementById("pytanie").innerHTML = question;
+      setCounter(Counter + 1);
+      setQuestion(listOfQuestions[Counter]);
+      setAnswer(listOfAnswers[Counter]);
+      let pytanie_here = document.getElementById("pytanie").innerHTML = Question;
+      let answer_here = document.getElementById("answer_box").value = "";
     }
-    return i
   }
-  
   return (
     <section id='all'>
-      <h2>Points:{points}</h2>
-      <h1 id="pytanie">{question}</h1>
+      <h2>Points:{Points}</h2>
+      <h1 id="pytanie">{Question}</h1>
       <form>
         <input id='answer_box' type='text'/>
         <br/>
-        <input id="submit_answer" type='button' value="Dalej" onClick={next}/>
+        <input id="submit_answer" type='button' value="Dalej" onClick={nextQuestion}/>
         <input id="startOrReset" type='button' value="Start/Reset" onClick={showTime}/>
       </form>
 
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
