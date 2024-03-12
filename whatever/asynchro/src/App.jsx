d8d65e4c33bc3ea50edf7219d63136f2b93b8a20
Jakub Majewski <jakub.majewski@cyfroweszkoly.pl>
2024-03-12 15:51:16 +0100

    added a button

--- whatever/asynchro/src/App.jsx
+++ whatever/asynchro/src/App.jsx
@@ -6,28 +6,47 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  async function fetchedCharacters(){
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await res.json()
+    const characters = data.results.map(character => {
+      return {
+        id: character.id,
+        name: character.name,
+        gender: character.gender,
+        species: character.species,
+        status: character.status
+      }
+    })
+    console.log(characters)
+    // fetch("https://rickandmortyapi.com/api/character")
+    // .then(res=>{
+    //   return res.json()
+    // })
+    // .then(data=> {
+    //   console.log(data)
+    //   const characters = data.results.map(character => {
+    //     return {
+    //       id: character.id,
+    //       name: character.name,
+    //       gender: character.gender,
+    //       species: character.species,
+    //       status: character.status
+    //     }
+    //   })
+    //   console.log(characters)
+    // })
+    // .catch(err=>{console.log(err)})
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <button onClick={fetchedCharacters}>Fetch Characters</button>
+      </section>
+
+      <section>
+        <p>To jest miejsce na wyświetlanie informacji o postaciach</p>
+      </section>
     </>
   )
 }
